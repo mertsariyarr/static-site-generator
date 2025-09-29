ab807fa675b284412ee75afccbe7f4f04b201833
mertsariyar <mertsariyar2@gmail.com>
2025-09-29 21:50:25 +0300

    I used all functions to convert markdown-flavored text into a list of TextNode objects

---
+++ src/test_text_to_nodes.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+from text_and_nodes import text_to_textnodes
+
+class TestTextToNode(unittest.TestCase):
+    def test_all_them(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGES, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                        ], result
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/text_and_nodes.py
@@ -0,0 +1,19 @@
+from textnode import TextNode, TextType
+from split_delemeter import split_nodes_delimiter
+from split_nodes_extracter import split_nodes_image, split_nodes_link
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
+node = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+
+if __name__ == "__main__":
+    text_to_textnodes(node)
