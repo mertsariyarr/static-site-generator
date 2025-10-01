67123fb60e11967a04bb5feea6388b928152f1ad
mertsariyar <mertsariyar2@gmail.com>
2025-10-01 21:11:12 +0300

    We need a way to inspect a block of markdown text and determine what type of block it is.

---
+++ src/block_types.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+""""
+paragraph
+heading
+code
+quote
+unordered_list
+ordered_list
+"""
+
+hd = "> list\n> items"
+print(hd)
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if len(lines) > 1 and lines[0].startswith(("```")) and lines[-1].startswith(("```")):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+
+print(block_to_block_type(hd))
+
+
+
+
